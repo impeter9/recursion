c54c0e651de373c267b765b39e6720b04dc16e24
Peter Park <impeter9@gmail.com>
2019-10-15 13:47:03 -0700

    complete both requirements and pass all the test cases

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var arr = [];
+	var node = document;
+	var getNodes = function(node, className){
+		// stopping case
+		if (node.childNodes.length === 0){
+
+		} else {
+			for (var i = 0; i<node.childNodes.length; i++){
+				if (_.contains(node.childNodes[i].classList, className)){
+					arr.push(node.childNodes[i]);
+				}
+			getNodes(node.childNodes[i], className);
+			}
+		}
+	}
+	getNodes(node,className)
+	return arr
 };
