3332afd08d81bd9072695142d1127ed1644f1a2c
Peter Park <impeter9@gmail.com>
2019-10-14 21:15:12 -0700

    complete the stringifyJSON portion and pass all test cases

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,44 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // base cases
+  if (typeof(obj) === "string") {
+  	return '"' + obj + '"'
+  } else if (typeof(obj) === "number") {
+  	return obj.toString()
+  } else if (typeof(obj) === "boolean"){
+  	return obj.toString()
+  } else if (obj === null){
+  	return "null"
+  } else if (typeof(obj) === "undefined"){
+  	return ""
+  } else if (typeof(obj) === "function"){
+  	return ""
+  }
+
+  //check if it's an array
+  if (Array.isArray(obj)) {
+  	var str = "";
+  	for (var i = 0; i < obj.length; i++){
+  		if (i === (obj.length - 1)) {
+  			str += stringifyJSON(obj[i]);
+  		} else {
+  			str += stringifyJSON(obj[i]) + ",";
+  		}
+  	}
+  	return "[" + str + "]"
+  } 
+  //check if it's an object
+  else if (typeof(obj) === "object") {
+  	var str = "";
+  	for (var key in obj){
+  		if (stringifyJSON(obj[key]) === ""){
+  			str += "";
+  		} else {
+  			str += ('"' + key + '":' + stringifyJSON(obj[key]) + ",");
+  		}
+  	}
+  	return "{" + str.slice(0,str.length-1) + "}"
+  }
+  
 };
